63b836be279a2669e22405009e45bed06bcbca32
Milan Neubert <milan.neubert@siemens.com>
2021-10-20 14:12:16 +0200

    Remove obsolete file. Fix correct copy function. (cp -> copy)

--- start.bat
+++ start.bat
@@ -34,7 +34,7 @@ if not exist winEnvCfg.bat (
   echo First run:
   echo     Creating winEnvCfg.bat file. Probably you have to fill it.
   echo.
-  cp winEnvCfgTemplate.bat winEnvCfg.bat
+  copy winEnvCfgTemplate.bat winEnvCfg.bat
 )
 call winEnvCfg.bat
 
