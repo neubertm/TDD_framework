dca5f4f32f989b402e246205db0a72f54982703b
Martin Ruzek <martin.ruzek@siemens.com>
2021-10-22 18:44:24 +0200

    CircBuff implementation & tests improved

--- project/CircBuff/CircBuff.hpp
+++ project/CircBuff/CircBuff.hpp
@@ -11,16 +11,18 @@ public:
   virtual ~CircBuff();
 
   bool push(const T[], int32_t i32_len);
+  bool push(const T);
   T pull();
   bool isNonEmpty() const;
 
-  static const int32_t ci32_mMaxBufferContent  = N;
 private:
+  static const int32_t ci32_mMaxBufferContent  = N;
   static const int32_t ci32_mRealBufferSize = ci32_mMaxBufferContent + 1;
+  
+  void add(const T);
 
   T a_mBuffer[ci32_mRealBufferSize];
   int32_t i32_mTail;
   int32_t i32_mHead;
 
-  void push(const T);
 };
