37378b0548bb5d0d74e1d5c7cf02baf187429a39
Milan Neubert <milan.neubert@siemens.com>
2021-11-22 15:41:08 +0100

    Added empty template file for creating new module

---
+++ Tools/defaults/src_templates/c_file.h
@@ -0,0 +1,19 @@
+/*!****************************************************************************
+ *  \brief      This header file contains definition of
+ *              ... class
+ *
+ *  \file       %%FILENAME.hpp
+ *
+ *  \author     XYz, X Yz, X.Yz@siemens.com
+ *
+ *  \date       %DATE
+ *
+ *  \copyright  Copyright (C) 2019 Siemens AG. All rights reserved.
+ *****************************************************************************/
+
+#ifndef %FILENAME_hpp_INCLUDED
+#define %FILENAME_hpp_INCLUDED
+
+int exampleFnc();
+
+#endif // Emitter%PACKAGENAME_%FILENAME_hpp_INCLUDED
